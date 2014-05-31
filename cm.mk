1535ff3ef4e2280f7269ef56db364cd0ed608c26
BobZhome <BobZhome@cheerful.com>
2014-05-31 09:18:39 -0400

    cm.mk: remove MetroPCS info

--- cm.mk
+++ cm.mk
@@ -10,14 +10,8 @@ $(call inherit-product, device/lge/p769/p769.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p769
 PRODUCT_NAME := cm_p769
-PRODUCT_BRAND := MetroPCS
-PRODUCT_MODEL := LGMS769
+PRODUCT_MODEL := LG769
 PRODUCT_MANUFACTURER := LGE
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="MetroPCS/u2_mpcs_us/u2:4.1.2/JZO54K/LGMS76910b.4DBC6301:user/release-keys" \
-    PRIVATE_BUILD_DESC="u2_mpcs_us-user 4.1.2 JZO54K LGMS76910b.4DBC6301 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
